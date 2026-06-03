0e74bdb589aea26ae8ef6b81d12657bff5e31523
Chris-xiny <Xinycs@outlook.com>
2026-06-04 00:18:34 +0800

    Interceptor拦截器与AOP基础(实现Service层各方法的耗时记录)

--- Web-Project/tlias-web-management/src/main/java/com/chrisxin/filter/TokenFilter.java
+++ Web-Project/tlias-web-management/src/main/java/com/chrisxin/filter/TokenFilter.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 
 @Slf4j
-@WebFilter("/*")
+//@WebFilter("/*")  //注释掉过滤器请求路径。暂时不用fliter
 public class TokenFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
---
+++ Web-Project/tlias-web-management/src/main/java/com/chrisxin/interceptor/TokenInterceptor.java
@@ -0,0 +1,47 @@
+package com.chrisxin.interceptor;
+
+import com.chrisxin.utils.JwtUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+@Slf4j
+public class TokenInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //获取请求头的token
+        String token = request.getHeader("token");
+        //判断token是否存在，不存在->响应401
+        if (token == null|| token.isEmpty()) {
+            log.info("请求token令牌不存在");
+            response.setStatus(401);
+            return false;
+        }
+        //解析token，校验失败->响应401
+        try {
+            JwtUtils.parseToken(token);
+        } catch (Exception e) {
+            log.info("令牌非法");
+            response.setStatus(401);
+            return false;
+        }
+        //校验通过放行
+        log.info("令牌合法");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
